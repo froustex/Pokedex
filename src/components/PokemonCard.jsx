35211fd42c7f5fdb5ec9b0a609eb0ee00feaa4fa
froustex <stephane-feroussier_student2022@wilder.school>
2024-04-08 17:15:40 +0200

    branch components.2 et affichage conditionnel

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,29 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard() {
+    const pokemon = {
+        name: "mew",
+        imgSrc:
+          "",
+      };
+
     return (
     <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image de pokemon" />        
-    <figcaption>Bulbasaur</figcaption>
-        
-    </figure>
+        {pokemon.imgSrc ?
+        <img src={pokemon.imgSrc} alt="image de pokemon" /> :
+        <p>???</p>
+        }      
+    <figcaption>{pokemon.name}</figcaption>   
+    </figure> 
     )
 }
 
