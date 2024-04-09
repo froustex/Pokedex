94c315fd72ad24342c4611d3cc3cd3a388e8111e
froustex <stephane-feroussier_student2022@wilder.school>
2024-04-09 15:51:53 +0200

    passage de props de parent vers enfant

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,6 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard() {
-    const pokemon = {
-        name: "mew",
-        imgSrc:
-          "",
-      };
+function PokemonCard({pokemon}) {
+  console.log(pokemon)
 
     return (
     <figure>
